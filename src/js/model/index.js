6d0fc94029d78e100bd2a1a94c1e9f542377fe1f
SF-Terry <547522203@qq.com>
2017-08-01 00:15:21 +0800

    Update fetch

--- src/js/model/index.js
+++ src/js/model/index.js
@@ -1,5 +1,5 @@
-import fetch from 'node-fetch'
+import '../util/fetch'
 
-export const FETCH_BLOG_DATA = () => fetch(`${location.origin}/data/blog/blogData.json`)
+export const FETCH_BLOG_DATA = () => window.fetch(`${location.origin}/data/blog/blogData.json`)
 
-export const FETCH_BLOG_DETAIL = path => fetch(`${location.origin}${path}`)
+export const FETCH_BLOG_DETAIL = path => window.fetch(`${location.origin}${path}`)
